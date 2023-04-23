ef9a8ab9a06335bc9d97acdf9df4cab7fdc9c504
Aliaksei Karneyeu <aliaksei.karneyeu@openx.com>
2023-04-24 01:20:31 +0200

    remove unused lsp languages

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -22,7 +22,7 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'terraformls', 'lua_ls', 'gopls','bashls', 'jsonnet_ls' }
+local servers = { 'pyright', 'terraformls', 'lua_ls', 'gopls', 'bashls', 'jsonnet_ls' }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
