2fbba8dafbd0026208d3a530b103582d764963e8
Aliaksei Karneyeu <aliaksei.karneyeu@openx.com>
2023-04-24 10:59:51 +0200

    make copilot work with cmp

--- nvim/lua/config/cmp/init.lua
+++ nvim/lua/config/cmp/init.lua
@@ -1,10 +1,19 @@
 -- nvim-cmp setup
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
+local lspkind = require 'lspkind'
 
-local symbols = require("plugins.completion_plugins.cmp_configs.symbols")
+lspkind.init {
+  symbol_map = {
+    Copilot = "",
+  },
+}
 
 cmp.setup {
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body)
@@ -39,7 +48,14 @@ cmp.setup {
   },
   sources = {
     { name = "copilot", group_index = 2 },
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
+    { name = 'nvim_lsp', group_index = 2 },
+    { name = 'luasnip', group_index = 2 },
   },
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+    })
+  }
 }
