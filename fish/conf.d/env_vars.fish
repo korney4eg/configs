1fd82ba0f4cd882aa2c274bb6b42a42579eb73f3
Aliaksei Karneyeu <korneevayu@gmail.com>
2025-04-11 21:11:01 +0200

    update fish

--- fish/conf.d/env_vars.fish
+++ fish/conf.d/env_vars.fish
@@ -1,2 +1,6 @@
 set EDITOR nvim
 export EDITOR
+
+export BUILDKIT_PROGRESS=plain
+
+export GOPRIVATE=github.com/openx
--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -1,6 +1,9 @@
 fish_add_path -g ~/projects/go-workspace/bin/
 fish_add_path -g /usr/local/sbin 
+fish_add_path -g /sbin 
 fish_add_path -g /opt/homebrew/opt/coreutils/libexec/gnubin
+fish_add_path -g /opt/homebrew/share/google-cloud-sdk/bin/
 fish_add_path -g ~/go/bin 
 fish_add_path -g /opt/homebrew/bin
 fish_add_path -g ~/configs/scripts
+fish_add_path -g ~/.docker/bin/
